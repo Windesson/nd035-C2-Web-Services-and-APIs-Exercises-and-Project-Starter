7ef43cba017d4afa16d18cb27a621b0fd5499b7e
Silva <windesson@hotmail.com>
2021-03-15 08:54:00 -0400

    one additional test has been implemented outside of contextLoads()

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/repository/PriceRepository.java
@@ -1,8 +1,9 @@
 package com.udacity.pricing.repository;
 
 import com.udacity.pricing.entity.Price;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PriceRepository extends CrudRepository<Price, Long> {
+public interface PriceRepository extends JpaRepository<Price, Long> {
 
 }
