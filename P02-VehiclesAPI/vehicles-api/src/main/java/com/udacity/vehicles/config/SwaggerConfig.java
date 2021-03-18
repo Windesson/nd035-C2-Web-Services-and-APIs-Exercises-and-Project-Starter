f455aed4dc1dc175bfcedd3fdd8c21c1a0fe934b
Silva <windesson@hotmail.com>
2021-03-18 09:08:41 -0400

    add swagger

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/config/SwaggerConfig.java
@@ -1,10 +1,8 @@
 package com.udacity.vehicles.config;
 
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.ResponseHeader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -21,10 +19,11 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
                 .paths(PathSelectors.any())
                 .build()
-                .useDefaultResponseMessages(false);
+                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo());
     }
 
     private ApiInfo apiInfo() {
